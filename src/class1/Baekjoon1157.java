54047747268e4e8b69cf564c6bfb2ba6819c8024
hansunghyeon <hsh5349@naver.com>
2022-10-08 03:50:23 +0900

    10.08

---
+++ src/class1/Baekjoon1157.java
@@ -0,0 +1,19 @@
+package class1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+//1157 단어 공부
+public class Baekjoon1157 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] arr = br.readLine().toUpperCase().split("");
+        Arrays.sort(arr);
+
+
+
+
+    }
+}
