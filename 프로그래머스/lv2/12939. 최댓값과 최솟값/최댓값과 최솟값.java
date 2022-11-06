40c143076926495a4f1962072ae7fac167ac5dd3
Han5ung <66876894+HanSungHyeon@users.noreply.github.com>
2022-11-07 02:29:37 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 3.84 ms, Memory: 76.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+class Solution {
+    public String solution(String s) {
+        int[] arr = Arrays.stream(s.split(" ")).mapToInt(Integer::parseInt).sorted().toArray();
+        
+        StringBuilder sb = new StringBuilder();
+        
+        sb.append(arr[0]).append(" ").append(arr[arr.length - 1]);
+        
+        return sb.toString();
+    }
+}
