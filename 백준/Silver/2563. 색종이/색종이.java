e6e0fe3f37d572ba7f7d809f8904eb044be6177e
Han5ung <66876894+HanSungHyeon@users.noreply.github.com>
2023-01-13 18:54:34 +0900

    [Silver V] Title: 색종이, Time: 124 ms, Memory: 14144 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		boolean[][] borad = new boolean[100][100];
+
+		int input = Integer.parseInt(br.readLine());
+
+		while (input-- > 0) {
+			st = new StringTokenizer(br.readLine());
+			int num1 = Integer.parseInt(st.nextToken());
+			int num2 = Integer.parseInt(st.nextToken());
+
+			for (int i = num1; i < num1 + 10; i++) {
+				for (int j = num2; j < num2 + 10; j++) {
+					borad[i][j] = true;
+				}
+			}
+		}
+		int result = 0;
+		for (int i = 0; i < borad.length; i++) {
+			for (int j = 0; j < borad.length; j++) {
+				if (borad[i][j])
+					result++;
+			}
+		}
+		System.out.println(result);
+	}
+}
