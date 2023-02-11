e6f03ed0872e462d57b02fd5cbbc9e878f786e63
Han5ung <66876894+HanSungHyeon@users.noreply.github.com>
2023-02-12 00:50:11 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 1816 ms, Memory: 106964 KB -BaekjoonHub

--- 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.java
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.java
@@ -1,45 +1,48 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static List<Integer>[] list;
-    public static int node, line, start, count = 0;
-    public static int[] flag;
+	static int[] flag;
+	static List<Integer>[] list;
+	static int count = 1;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        st = new StringTokenizer(br.readLine());
-        node = Integer.parseInt(st.nextToken());
-        line = Integer.parseInt(st.nextToken());
-        start = Integer.parseInt(st.nextToken());
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-        list = new ArrayList[node + 1];
-        flag = new int[node + 1];
-        for (int i = 1; i <= node; i++) list[i] = new ArrayList<>();
+		int node = Integer.parseInt(st.nextToken());
+		int edge = Integer.parseInt(st.nextToken());
+		int start = Integer.parseInt(st.nextToken());
 
-        while (line-- > 0) {
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
+		list = new LinkedList[node + 1];
+		flag = new int[node + 1];
 
-            list[a].add(b);
-            list[b].add(a);
-        }
-        for(int i = 1; i <= node; i++) Collections.sort(list[i]);
-        dfs(start);
-        for (int i = 1; i < flag.length; i++)
-            System.out.println(flag[i]);
-    }
+		for (int i = 0; i <= node; i++)
+			list[i] = new LinkedList<>();
 
-    public static void dfs(int start) {
-        flag[start] = ++count;
+		for (int i = 0; i < edge; i++) {
+			st = new StringTokenizer(br.readLine());
+			int from = Integer.parseInt(st.nextToken());
+			int to = Integer.parseInt(st.nextToken());
 
-        for (int i : list[start]) {
-            if(flag[i] == 0)
-                dfs(i);
-        }
-    }
+			list[from].add(to);
+			list[to].add(from);
+		}
+		for (int i = 0; i < node; i++)
+			Collections.sort(list[i]);
+
+		dfs(start);
+
+		for(int i = 1; i < flag.length; i++)
+			System.out.println(flag[i]);
+	}
+
+	public static void dfs(int node) {
+		flag[node] = count++;
+
+		for (int i : list[node]) {
+			if (flag[i] == 0)
+				dfs(i);
+		}
+	}
 }
