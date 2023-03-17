1eb2e1fa04116216f3c1a6898d7c6ef698c16b77
Han5ung <66876894+HanSungHyeon@users.noreply.github.com>
2023-03-17 23:03:33 +0900

    [Silver III] Title: 1로 만들기, Time: 120 ms, Memory: 31668 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,25 +1,46 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static int[] dp;
+    static int[] dp = new int[(int) (Math.pow(10, 6) + 1)];
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int input = Integer.parseInt(br.readLine()); //1이상 들어옴
-        dp = new int[input + 1];
 
-        dp[0] = dp[1] = 0;
-        
-        //재귀x -> 너무 오래걸림
-        //반복문 접근
-        //dp[idx]에 값 할당 후 최종 출력
-        for (int idx = 2; idx <= input; idx++) {
-            dp[idx] = dp[idx - 1] + 1; // -1 start
-            if(idx % 2 == 0) dp[idx] = Math.min(dp[idx], dp[idx / 2] + 1); //-1 start와 /2 start 중 최솟값
-            if(idx % 3 == 0) dp[idx] = Math.min(dp[idx], dp[idx / 3] + 1); //21번의 최솟값과 /3 start 중 최솟값
-        }
-        System.out.println(dp[input]);
+        int n = Integer.parseInt(br.readLine());
+
+        dp[1] = 0;
+        dp[2] = 1;
+        dp[3] = 1;
+
+        int ans = dp(n);
+
+        System.out.println(ans);
+    }
+
+    private static int dp(int num) {
+        if (num == 1) return dp[num];
+
+        if (num == 2) return dp[num];
+
+        if (num == 3) return dp[num];
+
+        //이전에 계산이 된 적 있는 값이라면
+        if (dp[num] > 0) return dp[num];
+
+        //이전에 연산된 적 없는 값이라면
+        int minus1 = Integer.MAX_VALUE, divide2 = Integer.MAX_VALUE, divide3 = Integer.MAX_VALUE;
+        if (num % 3 == 0) divide3 = dp(num / 3) + 1;
+
+        if (num % 2 == 0) divide2 = dp(num / 2) + 1;
+
+        minus1 = dp(num - 1) + 1;
+
+        int ans = Math.min(minus1, divide2);
+
+        ans = Math.min(ans, divide3);
+
+        return dp[num] = ans;
+
     }
 }
