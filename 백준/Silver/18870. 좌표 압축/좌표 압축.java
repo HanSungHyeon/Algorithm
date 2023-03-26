27e5a93dcf01d1bf110b450dd3545684e580fc54
Han5ung <66876894+HanSungHyeon@users.noreply.github.com>
2023-03-26 17:35:41 +0900

    [Silver II] Title: 좌표 압축, Time: 1856 ms, Memory: 355576 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.java
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -1,26 +1,44 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-
+	static int[] arr, clone;
+	static int n;
+	static StringBuilder sb = new StringBuilder();
+	static HashMap<Integer, Integer> map = new HashMap<>();
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		int input = Integer.parseInt(br.readLine());
+		n = Integer.parseInt(br.readLine());
+
+		// 원본 배열
+		arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+		// 배열 깊은 복사 후 오름차순 정렬
+		clone = arr.clone();
+		Arrays.sort(clone);
+
+		compression();
 		
-		int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-		int[] arr2 = Arrays.stream(arr).distinct().sorted().toArray();
-		
-		Map<Integer, Integer> map = new HashMap<>();
-		
-		for(int i = 0 ; i < arr2.length; i++) map.put(arr2[i], i);
-		
-		for(int el : arr) sb.append(map.get(el)).append(" ");
-		
+		print();
+	}
+	private static void print() {
+		for(int i : arr) {
+			sb.append(map.get(i)).append(" ");
+		}
 		System.out.println(sb);
+	}
+	
+	/*
+	 * 좌표 압축
+	 */
+	private static void compression() {
+		int rank = 0;
+		map.put(clone[0], rank);
+
+		for (int i = 1; i < n; i++) {
+			if(clone[i - 1] != clone[i]) 
+				rank++;
+			map.put(clone[i],rank);
+		}
 	}
 }
