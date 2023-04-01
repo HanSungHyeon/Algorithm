23f71ee6364756ac8b3f03f949e70dd0a3b4cfad
Han5ung <66876894+HanSungHyeon@users.noreply.github.com>
2023-04-02 04:58:56 +0900

    [Gold IV] Title: 주사위 굴리기, Time: 300 ms, Memory: 19284 KB -BaekjoonHub

--- 백준/Gold/14499. 주사위 굴리기/주사위 굴리기.java
+++ 백준/Gold/14499. 주사위 굴리기/주사위 굴리기.java
@@ -21,75 +21,63 @@ public class Main {
 	}
 
 	static int n, m, x, y, k;
-	static int[][] arr;
+	static int[][] arr, delta = { { 0, 0 }, { 0, 1 }, { 0, -1 }, { -1, 0 }, { 1, 0 } };
 	static Dice dice = new Dice(0, 0, 0, 0, 0, 0);
-	
-	//지도 값, 주사위 값 갱신
+
+	// 지도 값, 주사위 값 갱신
 	private static void replace() {
-		if(arr[x][y] == 0) arr[x][y] = dice.floor;
+		if (arr[x][y] == 0)
+			arr[x][y] = dice.floor;
 		else {
 			dice.floor = arr[x][y];
 			arr[x][y] = 0;
 		}
 	}
-	//주사위 굴리기
+
+	// 주사위 굴리기
 	private static void move(int num) {
 		int tmp = dice.floor;
-		//동쪽
-		if(num == 1) {
+		// 동쪽
+		if (num == 1) {
 			dice.floor = dice.right;
 			dice.right = dice.ceiling;
 			dice.ceiling = dice.left;
 			dice.left = tmp;
 		}
-		//서쪽
-		else if(num == 2) {
+		// 서쪽
+		else if (num == 2) {
 			dice.floor = dice.left;
 			dice.left = dice.ceiling;
 			dice.ceiling = dice.right;
 			dice.right = tmp;
 		}
-		//북쪽
-		else if(num == 3) {
+		// 북쪽
+		else if (num == 3) {
 			dice.floor = dice.top;
 			dice.top = dice.ceiling;
 			dice.ceiling = dice.bottom;
 			dice.bottom = tmp;
 		}
-		//남쪽
-		else if(num == 4) {
+		// 남쪽
+		else if (num == 4) {
 			dice.floor = dice.bottom;
 			dice.bottom = dice.ceiling;
 			dice.ceiling = dice.top;
 			dice.top = tmp;
 		}
-		
-		
+
 	}
-	
-	//주사위 굴리기가 가능한지 판단
-	//이동 가능하면 좌표 이동
+
+	// 주사위 굴리기가 가능한지 판단
+	// 이동 가능하면 좌표 이동
 	private static boolean isCheck(int num) {
-		//(동,1)
-		if(num == 1) {
-			if(y + 1 >= m) return false;
-			y++;
-		}
-		//(서,2)
-		else if(num == 2) {
-			if(y - 1 < 0) return false;
-			y--;
-		}
-		//(북,3)
-		else if(num == 3) {
-			if(x - 1 < 0) return false;
-			x--;
-		}
-		//(남,4)
-		else if(num == 4) {
-			if(x + 1 >= n) return false;
-			x++;
-		}
+		int dx = x + delta[num][0];
+		int dy = y + delta[num][1];
+		
+		if(dx < 0 || dy < 0 || dx >= n || dy >= m) return false;
+		
+		x = dx;
+		y = dy;
 		return true;
 	}
 
@@ -105,13 +93,13 @@ public class Main {
 		arr = new int[n][m];
 		for (int i = 0; i < n; i++)
 			arr[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-		
+
 		st = new StringTokenizer(br.readLine());
 		int size = st.countTokens();
 		for (int i = 0; i < size; i++) {
 			int num = Integer.parseInt(st.nextToken());
-			
-			if(isCheck(num)) {
+
+			if (isCheck(num)) {
 				move(num);
 				replace();
 				System.out.println(dice.ceiling);
