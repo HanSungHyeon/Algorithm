01c646ca106c7258b8fb3243bc51c1dcc1043c5c
Han5ung <66876894+HanSungHyeon@users.noreply.github.com>
2024-01-10 20:34:50 +0900

    [Silver II] Title: 최대 힙, Time: 408 ms, Memory: 29568 KB -BaekjoonHub

--- 백준/Silver/11279. 최대 힙/최대 힙.java
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -2,28 +2,28 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		/*
-		 * 입력으로 0이 들어오면 가장 큰 값 제거
-		 * 아니면 배열 삽입
-		 * 우선순위큐 사용 -> 정렬조건 내림차순
-		 */
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		PriorityQueue<Integer> pq = new PriorityQueue<>((o1,o2) -> o2-o1);
-		int n = Integer.parseInt(br.readLine());
-		
-		for(int i = 0 ; i < n; i++) {
-			int num = Integer.parseInt(br.readLine());
-			
-			if(num != 0) pq.add(num);
-				
-			else {
-				if(pq.isEmpty()) sb.append(0 + "\n");
-				
-				else sb.append(pq.poll()).append("\n");
-			}
-		}
-		System.out.println(sb);
-	}
+    public static PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2) -> o2 - o1);
+    public static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < n; i++) {
+            int num = Integer.parseInt(br.readLine());
+
+            if(num == 0) remove();
+            else insert(num);
+        }
+        System.out.println(sb.toString());
+    }
+    public static void insert(int num) {
+        pq.add(num);
+    }
+
+    public static void remove() {
+        if(pq.isEmpty()) sb.append(0);
+        else sb.append(pq.poll());
+        sb.append("\n");
+    }
 }
