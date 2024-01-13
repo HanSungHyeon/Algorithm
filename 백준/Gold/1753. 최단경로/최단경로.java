f0a7ad565017c57b1585fafaf943ccb7726062b6
Han5ung <66876894+HanSungHyeon@users.noreply.github.com>
2024-01-13 20:54:17 +0900

    [Gold IV] Title: 최단경로, Time: 808 ms, Memory: 119432 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -2,93 +2,71 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int v, e, start;
-	static List<Node>[] list;
-	static boolean[] flag;
-	static int[] distance;
+    public static class Node {
+        int node, d;
+        public Node(int node, int d) {
+            this.node = node;
+            this.d = d;
+        }
+    }
+    static int v, e, k, INF = Integer.MAX_VALUE;
+    static int[] dist;
+    static List<Node>[] list;
 
-	static class Vertex implements Comparable<Vertex> {
-		int n;
-		int w;
+    public static void main(String[] arsg) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-		public Vertex(int n, int w) {
-			this.n = n;
-			this.w = w;
-		}
+        v = Integer.parseInt(st.nextToken());
+        e = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(br.readLine());
+        dist = new int[v + 1];
+        list = new ArrayList[v + 1];
 
-		@Override
-		public int compareTo(Vertex o) {
-			return this.w - o.w;
-		}
-	}
-	
-	static class Node{
-		int to;
-		int weight;
-		public Node(int to, int weight) {
-			this.to = to;
-			this.weight = weight;
-		}
-	}
+        for(int i = 0; i < v + 1; i++) {
+            list[i] = new ArrayList<>();
+            dist[i] = INF;
+        }
+        dist[k] = 0;
 
-	private static void dijkstra() {
-		PriorityQueue<Vertex> pq = new PriorityQueue<>();
-		pq.offer(new Vertex(start, 0));
-		Arrays.fill(distance, Integer.MAX_VALUE);
-		distance[start] = 0;
-		while (!pq.isEmpty()) {
-			int cur = pq.peek().n;
-			pq.poll();
+        for(int i = 0; i < e; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
 
-			if (flag[cur])
-				continue;
+            list[u].add(new Node(v,w));
+        }
 
-			flag[cur] = true;
+        dijkstra();
+        StringBuilder sb = new StringBuilder();
+        for(int i = 1; i < v + 1; i++) {
+            if(dist[i] == INF) sb.append("INF");
+            else sb.append(dist[i]);
+            sb.append("\n");
+        }
 
-			for(Node node : list[cur]) {
-				int to = node.to;
-				int weight = node.weight;
-				if(!flag[to] && to != 0) {
-					distance[to] = Math.min(distance[to],distance[cur] + weight);
-					pq.offer(new Vertex(to,distance[to]));
-				}
-			}
-		}
-	}
+        System.out.println(sb);
+    }
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		v = Integer.parseInt(st.nextToken());
-		e = Integer.parseInt(st.nextToken());
+    public static void dijkstra(){
+        PriorityQueue<Node> pq = new PriorityQueue<>((o1, o2) -> o1.d - o2.d);
+        pq.add(new Node(k,0));
 
-		start = Integer.parseInt(br.readLine());
-		flag = new boolean[v + 1];
-		list = new ArrayList[v + 1];
-		for (int i = 1; i <= v; i++)
-			list[i] = new ArrayList<>();
+        while(!pq.isEmpty()) {
+            int node = pq.peek().node;
+            int d = pq.peek().d;
+            pq.poll();
 
-		distance = new int[v + 1];
-		for (int i = 0; i < e; i++) {
-			st = new StringTokenizer(br.readLine());
-			int from = Integer.parseInt(st.nextToken());
-			int to = Integer.parseInt(st.nextToken());
-			int weight = Integer.parseInt(st.nextToken());
+            if(dist[node] != d) continue; //확정된 거리와 다를 경우 걍 넘겨버림
 
-			list[from].add(new Node(to,weight));
-		}
-		dijkstra();
-//		System.out.println(Arrays.toString(distance));
-		for (int i = 1; i <= v; i++) {
-			if (distance[i] == Integer.MAX_VALUE)
-				sb.append("INF");
-			else
-				sb.append(distance[i]);
-			sb.append("\n");
-		}
+            for(Node n : list[node]) {
+                if(dist[n.node] > dist[node] + n.d) {
+                    dist[n.node] = dist[node] + n.d;
+                    pq.add(new Node(n.node, dist[n.node]));
+                }
+            }
 
-		System.out.println(sb);
-	}
-
+        }
+    }
 }
