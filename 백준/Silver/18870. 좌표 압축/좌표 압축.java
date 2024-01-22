db7d84e5cdc2f7b9c1a201c29ade4349089458e9
Han5ung <66876894+HanSungHyeon@users.noreply.github.com>
2024-01-22 22:14:32 +0900

    [Silver II] Title: 좌표 압축, Time: 1652 ms, Memory: 327064 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.java
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -2,43 +2,27 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int[] arr, clone;
-	static int n;
-	static StringBuilder sb = new StringBuilder();
-	static HashMap<Integer, Integer> map = new HashMap<>();
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		n = Integer.parseInt(br.readLine());
+    static int[] clone;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        clone = Arrays.stream(arr.clone()).distinct().sorted().toArray();
 
-		// 원본 배열
-		arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        for(int i : arr) {
+            sb.append(binarySearch(i, 0, clone.length - 1)).append(" ");
+        }
+        System.out.println(sb);
+    }
+    public static int binarySearch(int target, int start, int end) {
+        while(start <= end) {
+            int mid = (start + end) / 2;
 
-		// 배열 깊은 복사 후 오름차순 정렬
-		clone = arr.clone();
-		Arrays.sort(clone);
-
-		compression();
-		
-		print();
-	}
-	private static void print() {
-		for(int i : arr) {
-			sb.append(map.get(i)).append(" ");
-		}
-		System.out.println(sb);
-	}
-	
-	/*
-	 * 좌표 압축
-	 */
-	private static void compression() {
-		int rank = 0;
-		map.put(clone[0], rank);
-
-		for (int i = 1; i < n; i++) {
-			if(clone[i - 1] != clone[i]) 
-				rank++;
-			map.put(clone[i],rank);
-		}
-	}
+            if(target < clone[mid]) end = mid - 1;
+            else if (target > clone[mid]) start = mid + 1;
+            else return mid;
+        }
+        return 0;
+    }
 }
