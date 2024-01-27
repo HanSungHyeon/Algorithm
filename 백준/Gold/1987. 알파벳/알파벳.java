40f71f8dd7f8238e37756974d3f3954d6709b35b
Han5ung <66876894+HanSungHyeon@users.noreply.github.com>
2024-01-27 20:15:28 +0900

    [Gold IV] Title: 알파벳, Time: 724 ms, Memory: 14004 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -2,54 +2,47 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int[][] delta = { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } };
-	static boolean[] flag;
-	static String[][] arr;
-	static List<String> words = new ArrayList<>();;
-	static int r, c, ans = 0;
+    static int r, c, ans = 0;
+    static char[][] arr;
+    static int[] alpa = new int[26];
+    public static void main (String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        r = Integer.parseInt(st.nextToken());
+        c = Integer.parseInt(st.nextToken());
+        arr = new char[r][c];
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        for(int i = 0; i < r; i++) {
+            arr[i] = br.readLine().toCharArray();
+        }
 
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		r = Integer.parseInt(st.nextToken());
-		c = Integer.parseInt(st.nextToken());
+        dfs(0,0,1);
 
-		flag = new boolean[26];
-		arr = new String[r][c];
-		for (int i = 0; i < r; i++)
-			arr[i] = br.readLine().split("");
+        System.out.println(ans);
+    }
 
-		flag[arr[0][0].charAt(0) - 'A'] = true;
-		dfs(0, 0, 1);
-		System.out.println(ans);
-	}
+    static void dfs(int x, int y, int count) {
+        if(x < 0 || x >= r || y < 0 || y >= c || alpa[arr[x][y] - 'A'] > 0) {
+            ans = Math.max(ans, count - 1);
 
-	public static void dfs(int x, int y, int count) {
-		ans = Math.max(ans, count);
-		for (int i = 0; i < delta.length; i++) {
-			int dx = x + delta[i][0];
-			int dy = y + delta[i][1];
+            return;
+        }
 
-			if (dx < 0 || dy < 0 || dx >= r || dy >= c)
-				continue;
+        char c = arr[x][y];
+        alpa[c - 'A']++;
 
-			if (!flag[arr[dx][dy].charAt(0) - 'A']) {
-//				words.add(arr[dx][dy]);
-				flag[arr[dx][dy].charAt(0) - 'A'] = true;
-//				System.out.println(Arrays.toString(flag));
-				dfs(dx, dy, count + 1);
-				flag[arr[dx][dy].charAt(0) - 'A'] = false;
-//				words.remove(words.size() - 1);
-			}
-		}
-	}
+        //상
+        dfs(x - 1, y, count + 1);
 
-	public static boolean isCheck(String word) {
-		for (String s : words) {
-			if (word.equals(s))
-				return false;
-		}
-		return true;
-	}
+        //하
+        dfs(x + 1, y, count + 1);
+
+        //좌
+        dfs(x, y - 1, count + 1);
+
+        //우
+        dfs(x, y + 1, count + 1);
+
+        alpa[c - 'A']--;
+    }
 }
