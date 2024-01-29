f3fc6db0751f27c95fbf0c892ffbe5bd28864741
Han5ung <66876894+HanSungHyeon@users.noreply.github.com>
2024-01-29 21:06:30 +0900

    Update 숫자 카드 2.java

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -3,42 +3,46 @@ import java.util.*;
 
 public class Main {
     static int[] arr;
-    static HashMap<Integer,Integer> map = new HashMap<>();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        StringTokenizer st;
-
         int n = Integer.parseInt(br.readLine());
-        arr = new int[n];
-        st = new StringTokenizer(br.readLine(), " ");
-
-        for(int i = 0; i < n; i++) {
-            int num = Integer.parseInt(st.nextToken());
-            int count = map.getOrDefault(num, 0);
-
-            if(count == 0) arr[i] = num;
-            map.put(num, count + 1);
-        }
-        Arrays.sort(arr);
-
+        arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).sorted().toArray();
         int m = Integer.parseInt(br.readLine());
-        st = new StringTokenizer(br.readLine(), " ");
-        for(int i = 0; i < m; i++) {
-            int target = Integer.parseInt(st.nextToken());
-            sb.append(binarySearch(target, 0, arr.length - 1)).append(" ");
+        int[] tmp = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        for(int i : tmp) {
+            int s = firstBinarySearch(i);
+            int e = lastBinarySearch(i);
+            sb.append(e - s + 1).append(" ");
         }
         System.out.println(sb);
+
     }
-    public static int binarySearch(int target, int start, int end) {
+    //해당 숫자가 처음 나온 위치
+    static int firstBinarySearch(int target){
+        int start = 0;
+        int end = arr.length - 1;
+
         while(start <= end) {
             int mid = (start + end) / 2;
 
-            if(target < arr[mid]) end = mid - 1;
-            else if(target > arr[mid]) start = mid + 1;
-            else return map.get(target);
+            if(arr[mid] < target) start = mid + 1;
+            else end = mid - 1;
         }
+        return start;
+    }
 
-        return 0;
+    //해당 숫자가 마지막으로 나온 위치
+    static int lastBinarySearch(int target){
+        int start = 0;
+        int end = arr.length - 1;
+        while(start <= end) {
+            int mid = (start + end) / 2;
+
+            if(arr[mid] <= target) start = mid + 1;
+            else end = mid - 1;
+        }
+        return end;
     }
 }
