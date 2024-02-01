be52be8414d3da65908aa0257aa76139c83d2ba4
Han5ung <66876894+HanSungHyeon@users.noreply.github.com>
2024-02-01 17:26:44 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 252 ms, Memory: 18856 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -2,42 +2,22 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int[] arr;
-	static int[] d;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int[] d = new int[n];
+        int max = 1;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-		int n = Integer.parseInt(br.readLine());
-		arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-
-		d = new int[arr.length];
-		d[0] = 1;
-		for (int i = 1; i < arr.length; i++) {
-			if (arr[i] > arr[i - 1]) {
-				int max = Integer.MIN_VALUE;
-				for (int j = 0; j < i; j++) {
-					if (arr[j] < arr[i] && d[j] > max) {
-						max = d[j];
-						d[i] = d[j] + 1;
-//						System.out.println(d[i]);
-					}
-				}
-			} else {
-				int max = Integer.MIN_VALUE;
-				int count = 0;
-				for (int j = 0; j < i; j++) {
-					if (arr[j] < arr[i] && d[j] > max) {
-						max = d[j];
-						d[i] = d[j] + 1;
-					}
-					else count++;
-					
-					if(count == i) d[i] = 1;
-				}
-			}
-		}
-		//System.out.println(Arrays.toString(d));
-		System.out.println(Arrays.stream(d).max().getAsInt());
-	}
+        for(int i = 0; i < n; i++) {
+            d[i] = 1;
+            for(int j = 0; j < i; j++) {
+                if(arr[j] < arr[i]) {
+                    d[i] = Math.max(d[i], d[j] + 1);
+                }
+            }
+            max = Math.max(max, d[i]);
+        }
+        System.out.println(max);
+    }
 }
