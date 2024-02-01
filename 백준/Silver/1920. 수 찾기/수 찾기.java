e0e2774d6138788f97cbec798a3d24f2b9548f65
Han5ung <66876894+HanSungHyeon@users.noreply.github.com>
2024-02-01 14:39:18 +0900

    Update 수 찾기.java

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -2,33 +2,32 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		int[] a = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).sorted().toArray();
-		int m = Integer.parseInt(br.readLine());
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st = new StringTokenizer(br.readLine());
+    static int[] arr;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).sorted().toArray();
+        int m = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        while(m-- > 0) {
+            int target = Integer.parseInt(st.nextToken());
+            sb.append(binarySearch(target)).append("\n");
+        }
+        System.out.println(sb);
+    }
+    static int binarySearch(int target) {
+        int start = 0;
+        int end = arr.length - 1;
 
-		while (st.hasMoreTokens())
-			sb.append(binarySearch(a, Integer.parseInt(st.nextToken()))).append("\n");
-		System.out.println(sb);
-	}
+        while(start <= end) {
+            int mid = (start + end) / 2;
 
-	public static int binarySearch(int[] arr, int find) {
-		int start = 0;
-		int end = arr.length - 1;
+            if(arr[mid] == target) return 1;
 
-		while (start <= end) {
-			int mid = (start + end) / 2;
-
-			if (arr[mid] < find)
-				start = mid + 1;
-			else if (find < arr[mid])
-				end = mid - 1;
-			else
-				return 1;
-		}
-		return 0;
-	}
+            else if(arr[mid] < target) start = mid + 1;
+            else end = mid - 1;
+        }
+        return 0;
+    }
 }
