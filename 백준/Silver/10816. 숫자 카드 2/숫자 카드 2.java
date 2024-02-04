cf7a94ad4d82b0327f088b68e14206363e5f0129
Han5ung <66876894+HanSungHyeon@users.noreply.github.com>
2024-02-04 11:04:49 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1040 ms, Memory: 209328 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -2,47 +2,27 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int[] arr;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         int n = Integer.parseInt(br.readLine());
-        arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).sorted().toArray();
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for(int i = 0; i < n; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
         int m = Integer.parseInt(br.readLine());
-        int[] tmp = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        st = new StringTokenizer(br.readLine(), " ");
 
-        for(int i : tmp) {
-            int s = firstBinarySearch(i);
-            int e = lastBinarySearch(i);
-            sb.append(e - s + 1).append(" ");
+        for(int i = 0; i < m; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            int count = map.getOrDefault(num, 0);
+            sb.append(count).append(" ");
         }
+
         System.out.println(sb);
-
-    }
-    //해당 숫자가 처음 나온 위치
-    static int firstBinarySearch(int target){
-        int start = 0;
-        int end = arr.length - 1;
-
-        while(start <= end) {
-            int mid = (start + end) / 2;
-
-            if(arr[mid] < target) start = mid + 1;
-            else end = mid - 1;
-        }
-        return start;
-    }
-
-    //해당 숫자가 마지막으로 나온 위치
-    static int lastBinarySearch(int target){
-        int start = 0;
-        int end = arr.length - 1;
-        while(start <= end) {
-            int mid = (start + end) / 2;
-
-            if(arr[mid] <= target) start = mid + 1;
-            else end = mid - 1;
-        }
-        return end;
     }
 }
