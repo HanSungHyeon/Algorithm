9b075c01cd2c425399eaff7b0229fee45a0ea278
Han5ung <66876894+HanSungHyeon@users.noreply.github.com>
2024-02-06 22:03:12 +0900

    Update 숨바꼭질.java

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -2,64 +2,53 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static class Node {
-		int v;
-		int time;
+    static int n, k;
+    static int[] dist;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+        if(n == k) System.out.println(0);
+        else {
+            dist = new int[200001];
+            Arrays.fill(dist,Integer.MAX_VALUE);
+            bfs(n);
+            System.out.println(dist[k]);
+        }
+    }
 
-		public Node(int v, int time) {
-			this.v = v;
-			this.time = time;
-		}
-	}
+    static void bfs(int start) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(start);
+        dist[start] = 0;
 
-	static List<Integer>[] list;
-	static boolean[] flag;
-	static int n, k, ans = 0,tmp = 0;
+        while(!q.isEmpty()) {
+            int cur = q.poll();
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            if(isCheck(cur + 1) && min(cur, cur + 1)) {
+                q.add(cur + 1);
+            }
 
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		n = Integer.parseInt(st.nextToken());
-		k = Integer.parseInt(st.nextToken());
-		if(n > k) tmp = n;
-		else tmp = k;
-		list = new ArrayList[tmp + 2];
-		flag = new boolean[tmp + 2];
-		for (int i = n; i <= tmp + 1; i++)
-			list[i] = new ArrayList<>();
-		
-		bfs(n);
-		System.out.println(ans);
-	}
+            if(isCheck(cur - 1) && min(cur, cur - 1)) {
+                q.add(cur - 1);
+            }
 
-	public static void bfs(int node) {
-		Queue<Node> q = new LinkedList<>();
-		q.add(new Node(node, 0));
-		flag[node] = true;
+            if(isCheck(cur * 2) && min(cur, cur * 2)) {
+                q.add(cur * 2);
+            }
+        }
+    }
+    static boolean isCheck(int next) {
+        return next >= 0 && next < dist.length;
+    }
 
-		while (!q.isEmpty()) {
-			int v = q.peek().v;
-			int t = q.peek().time;
-			q.poll();
-//			System.out.println("v:" + v + " t:" + t);
-			if (v == k) {
-				ans = t;
-				break;
-			}
+    static boolean min(int cur, int next){
+        if(dist[next] > dist[cur] + 1) {
+            dist[next] = dist[cur] + 1;
+            return true;
+        }
+        return false;
+    }
 
-			if (v + 1 <= tmp + 1 && !flag[v + 1]) {
-				flag[v + 1] = true;
-				q.add(new Node(v + 1, t + 1));
-			}
-			if (v - 1 >= 0 && !flag[v - 1]) {
-				flag[v - 1] = true;
-				q.add(new Node(v - 1, t + 1));
-			}
-			if (v * 2 <= tmp + 1 && !flag[v * 2]) {
-				flag[v * 2] = true;
-				q.add(new Node(v * 2, t + 1));
-			}
-		}
-	}
 }
