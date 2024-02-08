5f0101a7bdee651b88b6170743fdf2b5528d6525
Han5ung <66876894+HanSungHyeon@users.noreply.github.com>
2024-02-08 20:51:37 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 572 ms, Memory: 144716 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -2,45 +2,51 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int n, m;
-	static List<Integer>[] list;
-	static boolean[] flag;
-	
-	private static void dfs(int node) {
-		flag[node] = true;
-		
-		for(int i : list[node]) {
-			if(!flag[i]) dfs(i);
-		}
-	}
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int n, m;
+    static boolean[] flag;
+    static List<Integer>[] list;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
 
-		StringTokenizer st = new StringTokenizer(br.readLine());
+        list = new ArrayList[n + 1];
+        flag = new boolean[n + 1];
+        for(int i = 1; i <= n; i++) list[i] = new ArrayList<>();
 
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-		flag = new boolean[n + 1];
-		list = new ArrayList[n + 1];
-		for(int i = 1; i <= n; i++)
-			list[i] = new ArrayList<>();
-		
-		for(int i = 0 ; i < m; i++) {
-			st = new StringTokenizer(br.readLine());
-			int from = Integer.parseInt(st.nextToken());
-			int to = Integer.parseInt(st.nextToken());
-			
-			list[from].add(to);
-			list[to].add(from);
-		}
-		int ans = 0;
-		for(int i = 1; i <= n; i++) {
-			if(!flag[i]) {
-				dfs(i);
-				ans++;
-			}
-		}
-		System.out.println(ans);
-	}
+        for(int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int from = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+
+            list[from].add(end);
+            list[end].add(from);
+        }
+        int count = 0;
+        for(int i = 1; i <= n; i++) {
+            if(!flag[i]) {
+                bfs(i);
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+    static void bfs(int start) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(start);
+        flag[start] = true;
+
+        while(!q.isEmpty()) {
+            int cur = q.poll();
+
+            for(int i : list[cur]) {
+                if(!flag[i]) {
+                    flag[i] = true;
+                    q.add(i);
+                }
+            }
+        }
+    }
+
 }
