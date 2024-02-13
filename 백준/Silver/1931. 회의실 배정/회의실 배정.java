a80fb14823f646e504cff0cae8a17b297f3bc69c
Han5ung <66876894+HanSungHyeon@users.noreply.github.com>
2024-02-14 03:06:10 +0900

    [Silver I] Title: 회의실 배정, Time: 1020 ms, Memory: 83040 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.java
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -2,34 +2,31 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
 		int n = Integer.parseInt(br.readLine());
-		
 		int[][] arr = new int[n][2];
-		// 입력
-		for (int i = 0; i < n; i++)
-			arr[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 
-		// 정렬
-		Arrays.sort(arr, new Comparator<int[]>() {
-			@Override
-			public int compare(int[] o1, int[] o2) {
-				if (o1[1] == o2[1])
-					return o1[0] - o2[0];
-				return o1[1] - o2[1];
-			}
-		});	
-		int ans = 1;
-		int start = arr[0][1];
-		for(int i = 1; i < n; i++) {
-			if(arr[i][0] >= start) {
-				start = arr[i][1];
-				ans++;
+		for (int i = 0; i < n; i++) {
+			arr[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+		}
+
+		Arrays.sort(arr, (o1, o2) -> {
+			if (o1[1] == o2[1]) return o1[0] - o2[0];
+
+			return o1[1] - o2[1];
+
+		});
+
+		int count = 1;
+		int endTime = arr[0][1];
+
+		for (int i = 1; i < n; i++) {
+			if (arr[i][0] >= endTime) {
+				endTime = arr[i][1];
+				count++;
 			}
 		}
-		System.out.println(ans);
+		System.out.println(count);
 	}
 }
