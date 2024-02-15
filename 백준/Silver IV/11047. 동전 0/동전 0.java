0dde899ac9b11afa5ddb0857d2f96a3792c38686
Han5ung <66876894+HanSungHyeon@users.noreply.github.com>
2024-02-15 23:15:59 +0900

    Update 동전 0.java

--- 백준/Silver IV/11047. 동전 0/동전 0.java
+++ 백준/Silver IV/11047. 동전 0/동전 0.java
@@ -1,35 +1,27 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+/*
+K원을 만드는데 필요한 동전 개수의 최솟값 출력
+ */
+
+import java.io.*;
+import java.util.*;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
 		int n = Integer.parseInt(st.nextToken());
 		int k = Integer.parseInt(st.nextToken());
 
-		int[] arr = new int[10];
-
+		int[] arr = new int[n];
 		for (int i = 0; i < n; i++) {
 			arr[i] = Integer.parseInt(br.readLine());
 		}
-
-		int count = 0;
+		
+		int ans = 0;
 		for (int i = n - 1; i >= 0; i--) {
-			if(k == 0) break;
-			
-			if(k >= arr[i]) {
-				int tmp = k / arr[i];
-				count += tmp;
-				k -= tmp * arr[i];
-			}
+			ans += (k / arr[i]);
+			k %= arr[i];
 		}
-		System.out.println(count);
+		System.out.println(ans);
 	}
-
 }
