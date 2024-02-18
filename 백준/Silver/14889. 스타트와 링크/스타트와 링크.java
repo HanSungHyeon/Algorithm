12e8c50c4f0b5fa244a5f280954deae7b33502fe
Han5ung <66876894+HanSungHyeon@users.noreply.github.com>
2024-02-18 17:56:56 +0900

    [Silver I] Title: 스타트와 링크, Time: 328 ms, Memory: 14980 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
@@ -2,77 +2,14 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int n;
+	static int n, ans = Integer.MAX_VALUE;
 	static int[][] arr;
-	static int[] result;
 	static boolean[] flag;
-	static boolean[] pflag;
-	static int[] presult;
-	static List<Integer> list;
-	static int sum = 0;
-	static int ans = Integer.MAX_VALUE;
-
-	// 팀 순열
-	private static void p(int num) {
-		if (num == 2) {
-			sum += arr[presult[0]][presult[1]];
-			return;
-		}
-
-		for (int i = 0; i < list.size(); i++) {
-			if (!pflag[i]) {
-				pflag[i] = true;
-				presult[num] = list.get(i);
-				p(num + 1);
-				pflag[i] = false;
-			}
-		}
-	}
-
-	// 스타트 링크 팀 조합
-	private static void c(int num, int start) {
-		if (num == n / 2) {
-			sum = 0;
-			Arrays.fill(flag, false);
-			presult = new int[2];
-			
-			for (int i : result)
-				flag[i] = true;
-			list = new ArrayList<>();
-
-			for (int i = 1; i < flag.length; i++) {
-				if (flag[i])
-					list.add(i);
-			}
-			pflag = new boolean[list.size()];
-			Arrays.fill(pflag, false);
-			p(0);
-			list = new ArrayList<>();
-			Arrays.fill(pflag, false);
-			int startT = sum;
-			sum = 0;
-			for (int i = 1; i < flag.length; i++) {
-				if (!flag[i])
-					list.add(i);
-			}
-			p(0);
-			int linkT = sum;
-			ans = Math.min(ans, Math.abs(startT - linkT));
-			return;
-		}
-
-		for (int i = start; i <= n; i++) {
-			result[num] = i;
-			c(num + 1, i + 1);
-		}
-	}
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
 		n = Integer.parseInt(br.readLine());
 		arr = new int[n + 1][n + 1];
-		result = new int[n / 2];
 		flag = new boolean[n + 1];
 		for (int i = 1; i <= n; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
@@ -80,9 +17,39 @@ public class Main {
 				arr[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-		c(0, 1);
+		c(1,n/2);
 		System.out.println(ans);
+	}
+
+	// 점수 계산
+	private static void score() {
+		int a = 0;
+		int b = 0;
+		for (int i = 1; i <= n - 1; i++) {
+			for (int j = i + 1; j <= n; j++) {
+				if (flag[i] && flag[j]) {
+					a += arr[i][j] + arr[j][i];
+				} else if (!flag[i] && !flag[j]) {
+					b += arr[i][j] + arr[j][i];
+				}
+			}
+		}
+		ans = Math.min(ans, Math.abs(a - b));
 
 	}
 
+	// 조합
+	private static void c(int start, int count) {
+		if (count == 0) {
+//			System.out.println(Arrays.toString(flag));
+			score();
+			return;
+		}
+
+		for (int i = start; i <= n; i++) {
+			flag[i] = true;
+			c(i + 1, count - 1);
+			flag[i] = false;
+		}
+	}
 }
