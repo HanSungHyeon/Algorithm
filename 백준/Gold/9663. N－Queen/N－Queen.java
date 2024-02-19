b2e4413643da0fa38b6546e576adaf272485b8b8
Han5ung <66876894+HanSungHyeon@users.noreply.github.com>
2024-02-19 15:42:09 +0900

    [Gold IV] Title: N-Queen, Time: 2708 ms, Memory: 14680 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -1,59 +1,67 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int n;
-    static int result = 0;
-    static int[][] delta = {{1, 0}, {1, -1}, {1, 1}};
+	static int n, count = 0;
+	static int[][] arr;
+	static int[][] delta = { { 1, -1 }, { 1, 0 }, { 1, 1 } };
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		n = Integer.parseInt(br.readLine());
+		arr = new int[n][n];
+		back(0);
+		System.out.println(count);
+	}
+	private static void print() {
+		for(int i =0 ;i < n; i++) {
+			System.out.println(Arrays.toString(arr[i]));
+		}
+		System.out.println("-----------------------------");
+	}
 
-          int[][] board = new int[n][n];
+	private static void back(int row) {
+		if(row == n) {
+			count++;
+//			print();
+			return;
+		}
+		
+		for (int i = 0; i < n; i++) {
+			// 퀸 놓을 수 있음
+			if (arr[row][i] == 0) {
+				arr[row][i]++;
+				// 공격로 표시
+				for (int d = 0; d < 3; d++) {
+					int dx = row;
+					int dy = i;
+					while (true) {
+						dx += delta[d][0];
+						dy += delta[d][1];
 
-        nQueen(0,board);
-        System.out.println(result);
-    }
+						if (dx < 0 || dy < 0 || dx >= n || dy >= n)
+							break;
 
-    public static void nQueen(int depth,int[][] board) {
-        if(depth == n){
-            result++;
-            return;
-        }
+						arr[dx][dy]++;
+					}
+				}
+				back(row + 1);
+				// 공격로 제거
+				arr[row][i]--;
+				for (int d = 0; d < 3; d++) {
+					int nx = row;
+					int ny = i;
+					while (true) {
+						nx += delta[d][0];
+						ny += delta[d][1];
 
-        for (int i = 0; i < n; i++) {
-            if(board[depth][i] == 0){
-                checkO(depth,i,board);
-                nQueen(depth + 1,board);
-                checkX(depth,i,board);
-            }
-        }
-    }
-    public static void checkO(int depth, int x,int[][] board){
-        board[depth][x]++;
-        for (int i = 0; i < delta.length; i++) {
-            for (int j = 1; j <= n; j++) {
-                int dx = x + delta[i][1] * j;
-                int dy = depth + delta[i][0] * j;
+						if (nx < 0 || ny < 0 || nx >= n || ny >= n)
+							break;
 
-                if(dx < 0 || dy < 0 || dx >= n || dy >= n) continue;
-                board[dy][dx]++;
-            }
-        }
-    }
-
-    public static void checkX(int depth, int x, int[][] board) {
-        board[depth][x]--;
-        for (int i = 0; i < delta.length; i++) {
-            for (int j = 1; j <= n; j++) {
-                int dx = x + delta[i][1] * j;
-                int dy = depth + delta[i][0] * j;
-
-                if(dx < 0 || dy < 0 || dx >= n || dy >= n) continue;
-                board[dy][dx]--;
-            }
-        }
-    }
+						arr[nx][ny]--;
+					}
+				}
+			}
+		}
+	}
 }
