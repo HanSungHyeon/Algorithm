8948d5cef28194656de12e5c9ccc211854749f33
Han5ung <hsh5349@naver.com>
2024-02-24 20:47:27 +0900

    Update 부분수열의 합.java

--- 백준/Silver II/1182. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver II/1182. 부분수열의 합/부분수열의 합.java
@@ -1,36 +1,44 @@
+//부분 수열의 합
+//순조부 부
+
 import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int n, s, ans = 0;
-    static int[] arr;
-    static boolean[] flag;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+	static int n, s, count = 0;
+	static int[] arr;
+	static boolean[] flag;
 
-        n = Integer.parseInt(st.nextToken());
-        s = Integer.parseInt(st.nextToken());
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        flag = new boolean[n];
+		n = Integer.parseInt(st.nextToken());
+		s = Integer.parseInt(st.nextToken());
+		arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+		flag = new boolean[n];
+		subSet(0);
 
-        combination(0,0,0);
-        System.out.println(ans);
-    }
+		if (s == 0) count--;
+		System.out.println(count);
+	}
 
-    static void combination(int start, int count, int sum) {
-        if(count == n) {
-            if(sum == s) ans++;
-            return;
-        }
+	static void subSet(int num) {
+		if (num == n) {
+			int sum = 0;
+			for (int i = 0; i < n; i++) {
+				if (flag[i]) sum += arr[i];
+			}
+			if (sum == s) count++;
+			return;
+		}
 
-        if(count > 0) {
-            if(sum == s) ans++;
-        }
+		//고르고 다음거 체크
+		flag[num] = true;
+		subSet(num + 1);
 
-        for(int i = start; i < n; i++) {
-            combination(i + 1, count + 1, sum + arr[i]);
-        }
-    }
+		//안고르고 다음거 체크
+		flag[num] = false;
+		subSet(num + 1);
+	}
 }
