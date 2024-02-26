f6fa45c0a1e4870cac68b02e1c3f43f8e06545e2
Han5ung <gksroenro@gmail.com>
2024-02-26 16:28:58 +0900

    [Silver III] Title: 1로 만들기, Time: 108 ms, Memory: 31712 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,46 +1,56 @@
+/*
+x % 3 == 0 -> 3으로 나눔
+x % 2 == 0 -> 2로 나눔
+x--;
+
+3가지 경우를 적절하게 사용해서
+1을 만들려고 함
+연산을 사용하는 횟수의 최소값 출력
+1000000
+완탐 -> n^3 -> 시간 초과
+시간 제한 : 0.15초
+2 to 1을 위해서는 최소 m회가 필요할거고~
+3 to 1을 위해서는 최소 m회가 필요할거고~
+n을 연산하다보면은 2, 3처럼 이미 계산 결과가 있는 애들이 있을거고~
+이미 계산 결과가 있으면 걍 그거 땡겨오면됨
+-> 굳이 2,3,...결과가 있는데 또 연산하러 갈 필요가 없음 -> dp
+ */
+
 import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int[] dp = new int[(int) (Math.pow(10, 6) + 1)];
+	static int n, INF = Integer.MAX_VALUE;
+	static int[] dp;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		n = Integer.parseInt(br.readLine());
+		dp = new int[n + 1];
+		System.out.println(dp(n));
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	}
+	static int dp(int num) {
+		if(num == 1) return 0;
+		if(num == 2 || num == 3) return 1;
 
-        int n = Integer.parseInt(br.readLine());
+		//이미 연산한 적이 있으면?
+		if(dp[num] > 0) return dp[num];
 
-        dp[1] = 0;
-        dp[2] = 1;
-        dp[3] = 1;
+		//3가지 경우
+		int tmp3 = INF, tmp2 = INF, tmp1 = INF;
+		//3으로 나누어 떨어질 때
+		if(num % 3 == 0) tmp3 = dp(num / 3);
 
-        int ans = dp(n);
+		//2로 나누어 떨어질 때
+		if(num % 2 == 0) tmp2 = dp(num / 2);
 
-        System.out.println(ans);
-    }
+		//1을 뺄 때
+		tmp1 = dp(num - 1);
 
-    private static int dp(int num) {
-        if (num == 1) return dp[num];
-
-        if (num == 2) return dp[num];
-
-        if (num == 3) return dp[num];
-
-        //이전에 계산이 된 적 있는 값이라면
-        if (dp[num] > 0) return dp[num];
-
-        //이전에 연산된 적 없는 값이라면
-        int minus1 = Integer.MAX_VALUE, divide2 = Integer.MAX_VALUE, divide3 = Integer.MAX_VALUE;
-        if (num % 3 == 0) divide3 = dp(num / 3) + 1;
-
-        if (num % 2 == 0) divide2 = dp(num / 2) + 1;
-
-        minus1 = dp(num - 1) + 1;
-
-        int ans = Math.min(minus1, divide2);
-
-        ans = Math.min(ans, divide3);
-
-        return dp[num] = ans;
-
-    }
+		//3개중 최소값 구하기
+		int ans = Math.min(tmp1, Math.min(tmp3, tmp2));
+		
+		//연산 한 번 추가 됨
+		return dp[num] = ans + 1;
+	}
 }
