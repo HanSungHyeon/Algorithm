15c561a5d6f975767e0e679dde668064d8a73eb2
Han5ung <gksroenro@gmail.com>
2024-02-27 20:06:46 +0900

    [Silver III] Title: 계단 오르기, Time: 76 ms, Memory: 11588 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -1,34 +1,41 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+/*
+1. 규칙
+ - 한 계단 또는 두 계단 오를 수 있음
+  - 연속된 세 개의 계단을 밟으면 안됨 -> 시작점은 계단에 포함되지 않음
+  - 마지막 계단은 무조건 밟아야함
+
+현재 위치에서 다음꺼를 밟거나 다다음꺼를 발거나 택해야함
+다음꺼를 밟으면 그 다음꺼는 자연스럽게 밟을 수 없음
+->
+ */
+
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static int[] stairs;
-    public static int[] dp;
+	static int[] arr, dp;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		arr = new int[n + 1];
+		dp = new int[n + 1];
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int num = Integer.parseInt(br.readLine());
+		for(int i = 1; i <= n; i++) arr[i] = Integer.parseInt(br.readLine());
 
-        stairs = new int[num + 1];
-        dp = new int[num + 1];
+		int ans = dp(n);
+		System.out.println(ans);
+	}
 
-        for (int i = 1; i <= num; i++)
-            stairs[i] = Integer.parseInt(br.readLine());
+	static int dp(int num) {
+		if(num == 1) return arr[num];
 
-        System.out.println(solution(num));
-    }
+		if(num == 2) return arr[num] + arr[num - 1];
 
-    public static int solution(int num) {
-        if(num == 1)
-            return dp[num] = stairs[num];
-        else if(num == 2)
-            return dp[num] = stairs[num] + stairs[num - 1];
-        else if (num == 3)
-            return dp[num] = Math.max(stairs[num - 1],stairs[num - 2]) + stairs[num];
-        else if (dp[num] > 0)
-            return dp[num];
-        else
-            return dp[num] = Math.max(solution(num - 2), solution(num - 3) + stairs[num - 1]) + stairs[num];
-    }
+		//3이면 점프 + 1, 1 + 점프 중 하나 골라야 함
+		if(num == 3) return Math.max(arr[1], arr[2]) + arr[3];
+
+		if(dp[num] > 0) return dp[num];
+
+		return dp[num] = Math.max(dp(num - 2), dp(num - 3) + arr[num - 1]) + arr[num];
+	}
 }
