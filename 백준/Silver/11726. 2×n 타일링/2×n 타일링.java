7b8819c0919c2c6d3f7eb02cd259c76cedb4c5a8
Han5ung <gksroenro@gmail.com>
2024-02-27 20:54:14 +0900

    [Silver III] Title: 2×n 타일링, Time: 76 ms, Memory: 11572 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -1,20 +1,23 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static int[] dp = new int[1001];
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int input = Integer.parseInt(br.readLine());
-
-        dp[0] = 0;
-        dp[1] = 1;
-        dp[2] = 2;
-
-        for (int i = 3; i <= input; i++)
-            dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
-
-        System.out.println(dp[input]);
-    }
+	static int[] dp;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		dp = new int[n  +1];
+		System.out.println(dp(n));
+		
+	}
+	private static int dp(int num) {
+		if(num == 1) return 1;
+		
+		if(num == 2) return 2;
+		
+		if(dp[num] > 0) return dp[num];
+		
+		return dp[num] = (dp(num - 1) + dp(num - 2)) % 10007;
+	}
 }
