b398eb7f6484c20de923def03238d354d85f6574
Han5ung <gksroenro@gmail.com>
2024-03-12 17:16:56 +0900

    [Silver I] Title: 군계일학, Time: 496 ms, Memory: 41644 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/15966. 군계일학/군계일학.java
+++ 백준/Silver/15966. 군계일학/군계일학.java
@@ -3,22 +3,20 @@ import java.util.*;
 
 public class Main {
 	static int n;
-	static int[] arr, dp;
+	static int[] arr;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		n = Integer.parseInt(br.readLine());
 		arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-		dp = new int[n];
+
+		HashMap<Integer, Integer> map = new HashMap<>();
 
 		int max = 0;
 		for (int i = 0; i < n; i++) {
-			dp[i] = 1;
-			for(int j = 0; j < i; j++) {
-				if(arr[i] - 1 == arr[j]) {
-					dp[i] = Math.max(dp[i], dp[j] + 1);
-				}
-			}
-			max = Math.max(dp[i], max);
+			int current = arr[i];
+			// 현재 값의 바로 이전 값이 map에 있다면, 이전 값의 최대 길이 + 1, 없다면 1
+			map.put(current, map.getOrDefault(current - 1, 0) + 1);
+			max = Math.max(max, map.get(current));
 		}
 		System.out.println(max);
 	}
