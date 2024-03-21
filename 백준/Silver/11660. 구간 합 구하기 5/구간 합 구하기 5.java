07bc0f3fa26ce01e9858a7e1c2d914bde850ce92
Han5ung <gksroenro@gmail.com>
2024-03-21 10:26:01 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 700 ms, Memory: 127284 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -3,28 +3,22 @@ import java.util.*;
 
 public class Main {
 	static int n, m;
-	static int[][] arr, dp;
+	static int[][] dp;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine()," ");
 		n = Integer.parseInt(st.nextToken());
 		m = Integer.parseInt(st.nextToken());
-		arr = new int[n + 1][n + 1];
+
 		dp = new int[n + 1][n + 1];
 
 		for(int i = 1; i <= n; i++) {
 			st = new StringTokenizer(br.readLine()," ");
 			for(int j = 1; j <= n; j++) {
-				arr[i][j] = Integer.parseInt(st.nextToken());
+				int num = Integer.parseInt(st.nextToken());
+				dp[i][j] = dp[i - 1][j] + dp[i][j - 1] + num - dp[i - 1][j - 1];
 			}
 		}
-
-		dp[0][0] = arr[0][0];
-		for(int i = 1; i <= n; i++) {
-			dp[1][i] = dp[1][i - 1] + arr[1][i];
-			dp[i][1] = dp[i - 1][1] + arr[i][1];
-		}
-		sum(n,n);
 
 		StringBuilder sb = new StringBuilder();
 		for(int i = 0; i < m; i++) {
@@ -37,14 +31,5 @@ public class Main {
 			sb.append(dp[x2][y2] - dp[x1 - 1][y2] - dp[x2][y1 - 1] + dp[x1 - 1][y1 - 1]).append("\n");
 		}
 		System.out.println(sb);
-	}
-	
-	//누적합 구하기
-	static int sum(int x, int y) {
-		if(x == 0 && y == 0) return dp[x][y];
-
-		if(dp[x][y] > 0) return dp[x][y];
-
-		return dp[x][y] = sum(x - 1, y) + sum(x, y - 1) + arr[x][y] - sum(x - 1, y - 1);
 	}
 }
