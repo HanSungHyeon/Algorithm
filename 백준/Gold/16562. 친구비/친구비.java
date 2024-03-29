af9457c85d9da2c59166cc1916b669b3fc776f1c
Han5ung <gksroenro@gmail.com>
2024-03-30 05:30:35 +0900

    [Gold IV] Title: 친구비, Time: 164 ms, Memory: 16932 KB -BaekjoonHub

--- 백준/Gold/16562. 친구비/친구비.java
+++ 백준/Gold/16562. 친구비/친구비.java
@@ -2,74 +2,64 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static class F {
-		int idx, cost;
-		public F(int idx, int cost) {
-			this.idx = idx;
-			this.cost = cost;
-		}
-	}
-	static int n,m,k, tmp;
-	static boolean[] flag;
-	static List<F> f = new ArrayList<>();
-	static List<Integer>[] list;
+	static int n,m,k;
+	static int[] parent, cost, p;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine()," ");
 		n = Integer.parseInt(st.nextToken());
 		m = Integer.parseInt(st.nextToken());
 		k = Integer.parseInt(st.nextToken());
-		flag = new boolean[n + 1];
-		tmp = k;
+
+		parent = new int[n + 1];
+		cost = new int[n + 1];
+		p = new int[n + 1];
+		for(int i = 1; i <= n; i++) parent[i] = i;
 
 		st = new StringTokenizer(br.readLine()," ");
 		for(int i = 1; i <= n; i++) {
-			f.add(new F(i, Integer.parseInt(st.nextToken())));
+			cost[i] = Integer.parseInt(st.nextToken());
 		}
 
-		list = new ArrayList[n + 1];
-		for(int i = 1; i <= n; i++) list[i] = new ArrayList<>();
-
+		//집합 만들기
 		for(int i = 0; i < m; i++) {
-			st = new StringTokenizer(br.readLine()," ");
-			int from = Integer.parseInt(st.nextToken());
-			int to = Integer.parseInt(st.nextToken());
+			st = new StringTokenizer(br.readLine(), " ");
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
 
-			list[from].add(to);
-			list[to].add(from);
+			//해당 집합에서 가장 적은 비용 get
+			union(a,b);
 		}
 
-		f.sort((o1,o2) -> o1.cost - o2.cost);
-
-		int count = 0;
-		for(F friend : f) {
-			if(friend.cost <= k && !flag[friend.idx]) {
-				count += bfs(friend.cost, friend.idx);
+		int ans = 0;
+		//루트 노드가 최소 금액임
+		for(int i = 1; i <= n; i++) {
+			if(parent[i] == i) {
+				ans += cost[i];
 			}
 		}
 		StringBuilder sb = new StringBuilder();
-		if(count == n) sb.append(tmp - k);
+		if(ans <= k) sb.append(ans);
 		else sb.append("Oh no");
+
 		System.out.println(sb);
 	}
-	static int bfs(int cost, int idx) {
-		Queue<Integer> q = new LinkedList<>();
-		flag[idx] = true;
-		k -= cost;
-		int count = 1;
-		q.add(idx);
+	static int find(int num) {
+		if(parent[num] == num) return num;
 
-		while(!q.isEmpty()) {
-			int cur = q.poll();
+		return parent[num] = find(parent[num]);
+	}
 
-			for(int num : list[cur]) {
-				if(flag[num]) continue;
-
-				flag[num] = true;
-				q.add(num);
-				count++;
+	static void union(int a, int b) {
+		a = find(a);
+		b = find(b);
+		
+		//합칠 때 비용 더 적은 쪽이 루트가 될 수 있게끔
+		if(a != b) {
+			if(cost[a] > cost[b]) {
+				parent[a] = b;
 			}
+			else parent[b] = a;
 		}
-		return count;
 	}
 }
