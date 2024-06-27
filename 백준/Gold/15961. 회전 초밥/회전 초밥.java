a4e9a359f04f2f2ef323baddc1fb50c517c2727f
Han5ung <gksroenro@gmail.com>
2024-06-28 01:57:50 +0900

    [Gold IV] Title: 회전 초밥, Time: 568 ms, Memory: 168688 KB -BaekjoonHub

---
+++ 백준/Gold/15961. 회전 초밥/회전 초밥.java
@@ -0,0 +1,63 @@
+/*
+* n <= 3,000,000
+* d <= 3,000
+* k <= 3,000
+* c <= 3,000
+* */
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	static int n, d, k, c, count, ans;
+	static int[] arr, flag;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+		n = Integer.parseInt(st.nextToken());
+		d = Integer.parseInt(st.nextToken());
+		k = Integer.parseInt(st.nextToken());
+		c = Integer.parseInt(st.nextToken());
+
+		arr = new int[n];
+		flag = new int[d + 1];
+
+		for(int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+
+		count = 0;
+		for(int i = 0; i < k; i++) {
+			if(flag[arr[i]] == 0) count++;
+			flag[arr[i]]++;
+		}
+
+		ans = count;
+		if(flag[c] == 0) ans++;
+
+		slide();
+		System.out.println(ans);
+	}
+	static void slide() {
+		for(int i = 1; i < n; i++) {
+			int idx = (i + k - 1) % n;
+
+			//시선 옮기기
+			if(flag[arr[idx]] == 0) count++;
+			flag[arr[idx]]++;
+
+			flag[arr[i - 1]]--;
+			if(flag[arr[i - 1]] == 0) count--;
+
+			findMax();
+		}
+	}
+
+	static void findMax() {
+		if(ans <= count) {
+			if(flag[c] == 0) ans++;
+			else ans = count;
+		}
+	}
+}
