9d19bcc4c7b0c0fa54bdc3af44c2ac75229b2d11
Han5ung <gksroenro@gmail.com>
2024-06-28 00:19:57 +0900

    [Silver I] Title: 회전 초밥, Time: 148 ms, Memory: 16404 KB -BaekjoonHub

--- 백준/Silver/2531. 회전 초밥/회전 초밥.java
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.java
@@ -1,49 +1,35 @@
 import java.io.*;
 import java.util.*;
+
 public class Main {
-	static int n, d, k, c;
-	static int[] arr;
-	static int[] flag;
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine()," ");
-		n = Integer.parseInt(st.nextToken());
-		d = Integer.parseInt(st.nextToken());
-		k = Integer.parseInt(st.nextToken());
-		c = Integer.parseInt(st.nextToken());
-		arr = new int[n];
-		flag = new int[d + 1];
-		for(int i = 0; i < n; i++) {
-			arr[i] = Integer.parseInt(br.readLine());
-		}
-		flag[c]++;
-		System.out.println(solution());
-	}
-	static int solution () {
-		int ans = 0;
-		//초기 작업
-		for(int i = 0; i < k; i++) {
-			flag[arr[i]]++;
-		}
-		ans = Math.max(isCheck(), ans);
-
-		//8 % 8 = 0
-		//logic
-		for (int i = 1; i < n; i++) {
-			flag[arr[i - 1]]--;
-			int idx = (i + k - 1) % n;
-			flag[arr[idx]]++;
-			ans = Math.max(isCheck(), ans);
-		}
-		return ans;
-	}
-
-	static int isCheck() {
-		int count = 0;
-		flag[c] = 1;
-		for(int i : flag) {
-			if(i > 0) count++;
-		}
-		return count;
-	}
+    static int N, d, k, c;
+    static int[] sushi;
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        d = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+        c = Integer.parseInt(st.nextToken())-1;
+        sushi = new int[N];
+        int[] eat = new int[d];	// 해당 종류의 초밥을 몇개 먹었는지 저장하는 배열
+        for(int i =0 ; i < N ; i++){
+            sushi[i] = Integer.parseInt(br.readLine())-1;
+        }
+        int res = 0;
+        int cnt = 0;
+        for(int i =0 ; i < k ; i++){
+            if(eat[sushi[i]]++ == 0) cnt++;
+        }
+        for(int i =0 ; i < N ; i++){
+            if(res <= cnt){     // MAX 값 새로 갱신
+                if(eat[c] == 0) res = cnt+1;
+                else res = cnt;
+            }
+            int j = (i+k)%N;  // end 값 (순환 → 인덱스 초과할 때의 처리)
+            if(eat[sushi[j]] ++ == 0) cnt++;
+            if(-- eat[sushi[i]] == 0) cnt--;
+        }
+        System.out.println(res);
+    }
 }
