2d56d6273f4842c8e35d2867af30f7c126d0bc51
Han5ung <gksroenro@gmail.com>
2024-06-29 02:39:00 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1424 ms, Memory: 278624 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
@@ -2,73 +2,74 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static List<Integer>[] list;
-	static boolean[]flag;
-	static int x, n, m, k;
-	static Queue<Node> q = new LinkedList<>();
-	static List<Integer> ans = new ArrayList<>();
 	static class Node {
-		int x;
-		int r;
-
-		public Node(int x, int r) {
-			this.x = x;
-			this.r = r;
+		int num, dist;
+		public Node(int num, int dist) {
+			this.num = num;
+			this.dist = dist;
 		}
 	}
 
-	public static void bfs(int start) {
-		
-		q.add(new Node(start, 0));
-		flag[start] = true;
-
-		while (!q.isEmpty()) {
-			int x = q.peek().x;
-			int dir = q.peek().r;
-			q.poll();
-			if(dir == k) ans.add(x);
-			for(int i : list[x]) {
-				if(!flag[i]) {
-					flag[i] = true;
-					q.add(new Node(i,dir + 1));
-				}
-			}
-		}
-	}
+	static final int INF = Integer.MAX_VALUE;
+	static int n, m, k, x;
+	static int[] dist;
+	static List<Node>[] list;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
 		n = Integer.parseInt(st.nextToken());
 		m = Integer.parseInt(st.nextToken());
 		k = Integer.parseInt(st.nextToken());
 		x = Integer.parseInt(st.nextToken());
 
-
-		flag = new boolean[n + 1];
 		list = new ArrayList[n + 1];
-		
-		for(int i = 1; i <= n; i ++)
+		dist = new int[n + 1];
+		Arrays.fill(dist, INF);
+		for (int i = 1; i <= n; i++) {
 			list[i] = new ArrayList<>();
-		
-		for (int i = 0; i < m; i++) {
-			st = new StringTokenizer(br.readLine());
+		}
 
+		for (int i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
 			int from = Integer.parseInt(st.nextToken());
 			int to = Integer.parseInt(st.nextToken());
 
-			list[from].add(to);
+			list[from].add(new Node(to, 1));
 		}
-		bfs(x);
-		
-		if(ans.size() == 0)
-			System.out.println(-1);
-		else {
-			ans.stream().sorted().forEach(v -> sb.append(v).append("\n"));
-			System.out.println(sb);
+		dijkstra(x);
+
+		StringBuilder sb = new StringBuilder();
+		boolean found = false;
+		for (int i = 1; i <= n; i++) {
+			if (dist[i] == k) {
+				sb.append(i).append("\n");
+				found = true;
+			}
 		}
-		
+		if (!found) sb.append(-1);
+		System.out.println(sb);
+	}
+
+	static void dijkstra(int start) {
+		PriorityQueue<Node> pq = new PriorityQueue<>(Comparator.comparingInt(o -> o.dist));
+		pq.add(new Node(start, 0));
+		dist[start] = 0;
+
+		while (!pq.isEmpty()) {
+			Node current = pq.poll();
+			int cur = current.num;
+			int d = current.dist;
+
+			if (d > dist[cur]) continue;
+
+			for (Node next : list[cur]) {
+				if (dist[next.num] > d + next.dist) {
+					dist[next.num] = d + next.dist;
+					pq.add(new Node(next.num, dist[next.num]));
+				}
+			}
+		}
 	}
 }
