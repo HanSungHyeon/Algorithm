dd9356f8e316d9597ec4738f8eb01d47d33c6bf8
Han5ung <gksroenro@gmail.com>
2024-06-29 02:39:21 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1388 ms, Memory: 285816 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
@@ -4,20 +4,18 @@ import java.util.*;
 public class Main {
 	static class Node {
 		int num, dist;
-		public Node(int num, int dist) {
+		public Node(int num , int dist) {
 			this.num = num;
 			this.dist = dist;
 		}
 	}
-
-	static final int INF = Integer.MAX_VALUE;
+	static int INF = Integer.MAX_VALUE;
 	static int n, m, k, x;
 	static int[] dist;
 	static List<Node>[] list;
-
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
 
 		n = Integer.parseInt(st.nextToken());
 		m = Integer.parseInt(st.nextToken());
@@ -26,12 +24,12 @@ public class Main {
 
 		list = new ArrayList[n + 1];
 		dist = new int[n + 1];
-		Arrays.fill(dist, INF);
-		for (int i = 1; i <= n; i++) {
+		for(int i = 1 ; i <= n; i++) {
+			dist[i] = INF;
 			list[i] = new ArrayList<>();
 		}
 
-		for (int i = 0; i < m; i++) {
+		for(int i =0 ; i < m; i++) {
 			st = new StringTokenizer(br.readLine(), " ");
 			int from = Integer.parseInt(st.nextToken());
 			int to = Integer.parseInt(st.nextToken());
@@ -40,32 +38,32 @@ public class Main {
 		}
 		dijkstra(x);
 
+		int ans = 0;
 		StringBuilder sb = new StringBuilder();
-		boolean found = false;
-		for (int i = 1; i <= n; i++) {
-			if (dist[i] == k) {
+		for(int i = 1; i <= n; i++) {
+			if(dist[i] == k) {
+				ans++;
 				sb.append(i).append("\n");
-				found = true;
 			}
 		}
-		if (!found) sb.append(-1);
+		if(ans == 0) sb.append(-1);
 		System.out.println(sb);
 	}
 
 	static void dijkstra(int start) {
-		PriorityQueue<Node> pq = new PriorityQueue<>(Comparator.comparingInt(o -> o.dist));
+		PriorityQueue<Node> pq = new PriorityQueue<>((o1, o2) -> o1.dist - o2.dist);
 		pq.add(new Node(start, 0));
 		dist[start] = 0;
 
-		while (!pq.isEmpty()) {
-			Node current = pq.poll();
-			int cur = current.num;
-			int d = current.dist;
+		while(!pq.isEmpty()) {
+			int cur = pq.peek().num;
+			int d = pq.peek().dist;
+			pq.poll();
 
-			if (d > dist[cur]) continue;
+			if(d != dist[cur]) continue;
 
-			for (Node next : list[cur]) {
-				if (dist[next.num] > d + next.dist) {
+			for(Node next : list[cur]) {
+				if(dist[next.num] > d + next.dist) {
 					dist[next.num] = d + next.dist;
 					pq.add(new Node(next.num, dist[next.num]));
 				}
